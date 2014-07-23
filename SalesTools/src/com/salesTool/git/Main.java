1df71599f09c01be0148ed820abba71bafd99036
shantha.lakshmi@compassitesinc.com <shantha.lakshmi@compassitesinc.com>
2014-07-23 11:54:43 +0530

    my first commit

---
+++ SalesTools/src/com/salesTool/git/Main.java
@@ -0,0 +1,21 @@
+package com.salesTool.git;
+
+public class Main {
+	
+	public static void main(String args[]) {
+	
+	SalesData data = new SalesData();
+	displayGreetings();
+	data.display();
+	}
+
+public static void displayGreetings() {
+	
+	System.out.println("HELLO HAPPY SALES PEOPLE");
+	System.out.println("THIS APP SHOWS SALES DATA");
+}
+
+}
+
+
+
---
+++ SalesTools/src/com/salesTool/git/SalesData.java
@@ -0,0 +1,22 @@
+package com.salesTool.git;
+
+public class SalesData {
+	
+	public static void main(String args[]) {
+		
+		int data[] = { 1, 4, 45};
+		
+	}
+		
+		public void display() {
+		
+		System.out.println("Impliment data display");
+		}
+		
+	}
+
+	
+		
+
+		
+		
